9e54a27e16200589c0c007a2eff99337ba37b29e
Arash Esfandiari <arashesfandiari@yahoo.com>
2024-04-09 18:55:36 -0400

    overlap fixed and logo added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
     return (
@@ -7,8 +8,7 @@ const Hero = () => {
             <div className="row">
                 <div className="col-12 col-md-8 offset-md-2">
                     <h1 className="display-4 fw-bold">BOOK FIVE SESSION, THE SIXTH IS ON US</h1>
-                    <button className="btn btn-dark btn-lg">REGISTER</button>
-                    <p>TILL 30.04.2023</p>
+                    <Link to="/appointment" className="btn btn-dark btn-lg">BOOK NOW</Link>
                 </div>
             </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Assuming your logo is located at src/assets/logo.png
+import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
             <div className="container-fluid">
-                {/* Left side - Book Appointment Button */}
-                <div className="col-3 col-lg-4 d-flex justify-content-start mb-3 mt-2">
+                {/* Left side - Book Appointment Button only visible on sm screens and up */}
+                <div className="d-none d-sm-flex col-lg-4 justify-content-start mb-3 mt-2">
                     <Link to="/appointment" className="btn btn-outline-dark text-nowrap">Book Appointment</Link>
                 </div>
 
-                {/* Centered Title */}
-                <div className="col-6 col-lg-4 d-flex justify-content-center">
+                {/* Centered Title with Logo */}
+                <div className="col-6 col-lg-4 d-flex justify-content-center align-items-center">
+                    {/* Logo Image */}
+                    <img src={logo} alt="Baby's Laser Clinic Logo" style={{ marginRight: '10px', height: '40px' }} />
                     <Link className="navbar-brand fw-bold fs-3" to="/">Baby's Laser Clinic</Link>
                 </div>
 
@@ -22,8 +26,10 @@ const Navbar = () => {
                 </button>
 
                 {/* Collapsible content */}
-                <div className="col-3 col-lg-4 collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav ms-auto">
+                        {/* Book Appointment Button inside the collapsible menu only visible on xs screens */}
+                        <Link to="/appointment" className="nav-link d-lg-none">Book Appointment</Link>
                         <Link className="nav-link active" to="/">Home</Link>
                         <Link className="nav-link" to="/services">Services</Link>
                         <Link className="nav-link" to="/about">About</Link>
