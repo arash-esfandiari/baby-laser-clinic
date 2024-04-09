bce5dc9405ec9178ac62e58706b43f6bc215c0a8
Arash Esfandiari <arashesfandiari@yahoo.com>
2024-04-09 19:30:09 -0400

    logo overflow fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,18 @@ import logo from '../assets/images/logo.png';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
-            <div className="container-fluid">
-                {/* Left side - Book Appointment Button only visible on sm screens and up */}
-                <div className="d-none d-sm-flex col-lg-4 justify-content-start mb-3 mt-2">
-                    <Link to="/appointment" className="btn btn-outline-dark text-nowrap">Book Appointment</Link>
-                </div>
+            <div className="container">
+                {/* Logo and Title container */}
+                <div className="d-flex align-items-center">
+                    {/* Logo */}
+                    <Link to="/" className="navbar-brand">
+                        <img src={logo} alt="Logo" className="d-inline-block align-top" style={{ maxHeight: '50px' }} />
+                    </Link>
 
-                {/* Centered Title with Logo */}
-                <div className="col-6 col-lg-4 d-flex justify-content-center align-items-center">
-                    {/* Logo Image */}
-                    <img src={logo} alt="Baby's Laser Clinic Logo" style={{ marginRight: '10px', height: '40px' }} />
-                    <Link className="navbar-brand fw-bold fs-3" to="/">Baby's Laser Clinic</Link>
+                    {/* Title */}
+                    <Link to="/" className="navbar-brand fw-bold fs-3 d-none d-lg-block">
+                        Baby's Laser Clinic
+                    </Link>
                 </div>
 
                 {/* Toggler */}
@@ -26,14 +27,20 @@ const Navbar = () => {
                 </button>
 
                 {/* Collapsible content */}
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav ms-auto">
-                        {/* Book Appointment Button inside the collapsible menu only visible on xs screens */}
-                        <Link to="/appointment" className="nav-link d-lg-none">Book Appointment</Link>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    {/* Book Appointment Button visible on sm screens and up */}
+                    <Link to="/appointment" className="btn btn-outline-dark text-nowrap d-none d-md-block me-2">
+                        Book Appointment
+                    </Link>
+
+                    {/* Navbar Links */}
+                    <div className="navbar-nav">
                         <Link className="nav-link active" to="/">Home</Link>
                         <Link className="nav-link" to="/services">Services</Link>
                         <Link className="nav-link" to="/about">About</Link>
                         <Link className="nav-link" to="/contact">Contact</Link>
+                        {/* Book Appointment Button inside the collapsible menu only visible on xs to md screens */}
+                        <Link to="/appointment" className="nav-link d-block d-md-none">Book Appointment</Link>
                     </div>
                 </div>
             </div>
