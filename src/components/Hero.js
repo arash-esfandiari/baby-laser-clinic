e70aaa88d2981b2ce85ff98325c753fff9c51afe
Arash Esfandiari <arashesfandiari@yahoo.com>
2024-04-09 19:37:11 -0400

    title added to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,13 @@ const Hero = () => {
         <div className="container-fluid bg-light text-center py-5">
             <div className="row">
                 <div className="col-12 col-md-8 offset-md-2">
-                    <h1 className="display-4 fw-bold">BOOK FIVE SESSION, THE SIXTH IS ON US</h1>
+                    {/* Website Title */}
+                    <h2 className="display-6 fw-bold text-dark mb-4 pb-2 border-bottom border-3 border-primary">
+                        Baby's Laser Clinic
+                    </h2>
+
+                    {/* Promotional Message */}
+                    <h1 className="display-4 fw-bold mb-3">BOOK FIVE SESSIONS, THE SIXTH IS ON US</h1>
                     <Link to="/appointment" className="btn btn-dark btn-lg">BOOK NOW</Link>
                 </div>
             </div>
