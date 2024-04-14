fb952a7921f6e1f65c95dcb8bc7b5ce3c4848e53
Arash Esfandiari <arashesfandiari@yahoo.com>
2024-04-14 17:19:48 -0400

    Mara Appointment Schedule Added

--- src/pages/AppointmentPage.js
+++ src/pages/AppointmentPage.js
@@ -7,11 +7,11 @@ const AppointmentPage = () => {
             <h2 className="mb-3">Book an Appointment</h2>
             {/* Google Calendar Appointment Scheduling begin */}
             <iframe
-                src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0HNG2bKEM2MpltOLSHEG6VZQqwnxMT187FQuA9X0Yjlgmra_7nJkHpQNAX2fJkRftzaFnKROui?gv=true"
+                src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1G46jwTfVgtfMCvxpOvxcbEd0HxngU6pcA9eVTf9uELWug4HniRvUEt6A_vt2XEr0Cpoka46nQ?gv=true"
                 style={{ border: 0 }}
                 width="100%"
                 height="600"
-                frameBorder="0"
+                frameborder="0"
                 title="Appointment Scheduling"
             ></iframe>
             {/* end Google Calendar Appointment Scheduling */}
