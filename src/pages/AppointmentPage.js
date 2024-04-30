d54ed5ca74e0b1ddcdff4c3b4c6eb44db26ef5b4
Arash Esfandiari <arashesfandiari@yahoo.com>
2024-04-30 00:59:48 -0400

    Account changed

--- src/pages/AppointmentPage.js
+++ src/pages/AppointmentPage.js
@@ -7,7 +7,7 @@ const AppointmentPage = () => {
             <h2 className="mb-3">Book an Appointment</h2>
             {/* Google Calendar Appointment Scheduling begin */}
             <iframe
-                src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1G46jwTfVgtfMCvxpOvxcbEd0HxngU6pcA9eVTf9uELWug4HniRvUEt6A_vt2XEr0Cpoka46nQ?gv=true"
+                src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1eNwEmBo78Be-NV9saNx6xygP9AzAoCp1T-aQxnvDqYQXkj5jKRGidscHC9jj2WdKGma-1TV3p?gv=true"
                 style={{ border: 0 }}
                 width="100%"
                 height="600"
